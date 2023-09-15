0d2cd19f52c08ff2ffa84535e5b52806df0b6ff7
MartinXCVI <mdzg96@gmail.com>
2023-09-15 18:27:34 -0300

    second commit of exercises

---
+++ Part1/LeftRight/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react"
+
+const History = (props) => {
+  if(props.allClicks.length === 0) {
+    return (
+      <div>
+        The App is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      Button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(updatedRight + left)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='Left' />
+      <Button handleClick={handleRightClick} text='Right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+export default App
--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,49 +1,40 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
+import { useState } from 'react'
 
-const Part = props => {
-  return <p>{props.part.name} {props.part.exercises}</p>
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Content = props => {
-  return <>
-    <Part part={props.parts[0]}  />
-    <Part part={props.parts[1]}  />
-    <Part part={props.parts[2]}  />
-  </>
-}
-
-const Total = (props) => {
-  console.log(props)
-  return <p>Number of exercises{' '} {props.parts.reduce((total, part) => part.exercises + total, 0)}</p>
-}
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
   }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)  
+    setCounter(0)
+  }
+
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text='Plus' />
+      <Button handleClick={setToZero} text='Zero' />
+      <Button handleClick={decreaseByOne} text='Decrease' />
     </div>
   )
 }
